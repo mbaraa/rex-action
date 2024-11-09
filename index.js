48b46883d68339dd173ae22d01f701cf68752e33
Baraa Al-Masri <pub@mbaraa.com>
2024-11-09 03:13:53 +0300

    Add commit hash

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const axios = require("axios");
 /**
  * @param{string} serverURL
  * @param{string} repoName
+ * @param{string} commitSha
  * @param{string} rexToken
  * @param{() => void} resultFunc
  *
  */
-function getBuildOutput(serverURL, repoName, rexToken, resultFunc) {
+function getBuildOutput(serverURL, repoName, commitSha, rexToken, resultFunc) {
   axios
-    .get(`${serverURL}/deploy/?name=${repoName}`, {
+    .get(`${serverURL}/deploy/?name=${repoName}&commit_sha=${commitSha}`, {
       headers: {
         Authorization: rexToken,
       },
@@ -27,10 +28,11 @@ function getBuildOutput(serverURL, repoName, rexToken, resultFunc) {
 try {
   const serverURL = core.getInput("server-url");
   const token = core.getInput("token");
+  const commitSha = core.getInput("commit-sha");
   const repoName = core.getInput("repo-name");
   console.log(`Deploying ${repoName} using Rex`);
 
-  getBuildOutput(serverURL, repoName, token, (result) => {
+  getBuildOutput(serverURL, repoName, commitSha, token, (result) => {
     if (result && result !== "error") {
       core.setOutput("Build succeeded");
       console.log(`Deploying ${repoName} was successful`);
